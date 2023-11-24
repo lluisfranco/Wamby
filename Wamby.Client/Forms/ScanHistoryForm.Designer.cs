6e380286a08a4aead4e449cc2217bce25a46d10d
Lluis Franco <hello@lluisfranco.com>
2023-11-24 10:03:43 +0100

    Added history form

---
+++ Wamby.Client/Forms/ScanHistoryForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿using Wamby.Client.Forms;
+
+namespace Wamby.Client
+{
+    partial class ScanHistoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            barManager = new DevExpress.XtraBars.BarManager(components);
+            bar = new DevExpress.XtraBars.Bar();
+            barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            svgImageCollectionForm = new DevExpress.Utils.SvgImageCollection(components);
+            ((System.ComponentModel.ISupportInitialize)barManager).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)svgImageCollectionForm).BeginInit();
+            SuspendLayout();
+            // 
+            // barManager
+            // 
+            barManager.Bars.AddRange(new DevExpress.XtraBars.Bar[] { bar });
+            barManager.DockControls.Add(barDockControlTop);
+            barManager.DockControls.Add(barDockControlBottom);
+            barManager.DockControls.Add(barDockControlLeft);
+            barManager.DockControls.Add(barDockControlRight);
+            barManager.Form = this;
+            barManager.MaxItemId = 4;
+            // 
+            // bar
+            // 
+            bar.BarName = "Tools";
+            bar.DockCol = 0;
+            bar.DockRow = 0;
+            bar.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            bar.Text = "Tools";
+            // 
+            // barDockControlTop
+            // 
+            barDockControlTop.CausesValidation = false;
+            barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            barDockControlTop.Manager = barManager;
+            barDockControlTop.Size = new System.Drawing.Size(1025, 20);
+            // 
+            // barDockControlBottom
+            // 
+            barDockControlBottom.CausesValidation = false;
+            barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            barDockControlBottom.Location = new System.Drawing.Point(0, 737);
+            barDockControlBottom.Manager = barManager;
+            barDockControlBottom.Size = new System.Drawing.Size(1025, 0);
+            // 
+            // barDockControlLeft
+            // 
+            barDockControlLeft.CausesValidation = false;
+            barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            barDockControlLeft.Location = new System.Drawing.Point(0, 20);
+            barDockControlLeft.Manager = barManager;
+            barDockControlLeft.Size = new System.Drawing.Size(0, 717);
+            // 
+            // barDockControlRight
+            // 
+            barDockControlRight.CausesValidation = false;
+            barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            barDockControlRight.Location = new System.Drawing.Point(1025, 20);
+            barDockControlRight.Manager = barManager;
+            barDockControlRight.Size = new System.Drawing.Size(0, 717);
+            // 
+            // svgImageCollectionForm
+            // 
+            svgImageCollectionForm.Add("History", "Wamby.Resources.Images.App.History.svg", "Wamby.Resources");
+            // 
+            // ScanHistoryForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1025, 737);
+            Controls.Add(barDockControlLeft);
+            Controls.Add(barDockControlRight);
+            Controls.Add(barDockControlBottom);
+            Controls.Add(barDockControlTop);
+            Name = "ScanHistoryForm";
+            Text = "History";
+            ((System.ComponentModel.ISupportInitialize)barManager).EndInit();
+            ((System.ComponentModel.ISupportInitialize)svgImageCollectionForm).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.BarManager barManager;
+        private DevExpress.XtraBars.Bar bar;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.Utils.SvgImageCollection svgImageCollectionForm;
+    }
+}
--- Wamby.Client/Forms/WelcomeForm.cs
+++ Wamby.Client/Forms/WelcomeForm.cs
@@ -18,11 +18,13 @@ namespace Wamby.Client
         public event EventHandler<EventArgs> NewScanClick;
         public event EventHandler<EventArgs> OpenScanClick;
         public event EventHandler<OpenPreviousScanEventArgs> OpenPreviousScanClick;
+        public event EventHandler<EventArgs> OpenHistoryClick;
 
         public void RaiseNewScanEvent() => NewScanClick?.Invoke(this, EventArgs.Empty);
         public void RaiseOpenScanEvent() => OpenScanClick?.Invoke(this, EventArgs.Empty);
         public void OpenPreviousScanEvent(string path, string pattern, ScanDetailTypeEnum type) =>
             OpenPreviousScanClick?.Invoke(this, new OpenPreviousScanEventArgs() { Path = path, SearchPattern = pattern, DetailType = type });
+        public void RaiseOpenHistoryEvent() => OpenHistoryClick?.Invoke(this, EventArgs.Empty);
 
         public WelcomeForm()
         {
@@ -32,6 +34,7 @@ namespace Wamby.Client
             WelcomeUserControl.NewScanClick += (s, e) => RaiseNewScanEvent();
             WelcomeUserControl.OpenScanClick += (s, e) => RaiseOpenScanEvent();
             WelcomeUserControl.OpenPreviousScanClick += (s, e) => OpenPreviousScanEvent(e.Path, e.SearchPattern, e.DetailType);
+            WelcomeUserControl.OpenHistoryClick += (s, e) => RaiseOpenHistoryEvent();
         }
     }
 }
